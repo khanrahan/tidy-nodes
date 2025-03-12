dc5e0d317f77d94ad818be7ba589ed6f2209e17f
Kieran Hanrahan <kieran@knhn.me>
2025-03-11 21:04:01 -0400

    Bump version

--- tidy_nodes.py
+++ tidy_nodes.py
@@ -2,7 +2,7 @@
 Script Name: Tidy Nodes
 Written By: Kieran Hanrahan
 
-Script Version: 2.0.0
+Script Version: 2.0.1
 Flame Version: 2025
 
 URL: http://github.com/khanrahan/tidy-nodes
@@ -35,7 +35,7 @@ import flame
 from PySide6 import QtCore, QtGui, QtWidgets
 
 TITLE = 'Tidy Nodes'
-VERSION_INFO = (2, 0, 0)
+VERSION_INFO = (2, 0, 1)
 VERSION = '.'.join([str(num) for num in VERSION_INFO])
 TITLE_VERSION = f'{TITLE} v{VERSION}'
 MESSAGE_PREFIX = '[PYTHON]'
